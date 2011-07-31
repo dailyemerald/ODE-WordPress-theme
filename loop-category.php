db27981111e76d10181cbf98c98e25d4a1bee62f
Daniel Bachhuber <d@danielbachhuber.com>
2011-07-31 16:22:59 +0000

    Add Co-Authors support across all templates

--- loop-category.php
+++ loop-category.php
@@ -1,24 +1,3 @@
-<?php
-/**
- * The loop that displays posts.
- *
- * The loop displays the posts and the post content.  See
- * http://codex.wordpress.org/The_Loop to understand it and
- * http://codex.wordpress.org/Template_Tags to understand
- * the tags used in it.
- *
- * This can be overridden in child themes with loop.php or
- * loop-template.php, where 'template' is the loop context
- * requested by a template. For example, loop-index.php would
- * be used if it exists and we ask for the loop with:
- * <code>get_template_part( 'loop', 'index' );</code>
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
-?>
-
 <?php /* Display navigation to next/previous pages when applicable */ ?>
 <?php if ( $wp_query->max_num_pages > 1 ) : ?>
 	<div id="nav-above" class="navigation">
@@ -38,29 +17,13 @@
 	</div><!-- #post-0 -->
 <?php endif; ?>
 
-<?php
-	/* Start the Loop.
-	 *
-	 * In Twenty Ten we use the same loop in multiple contexts.
-	 * It is broken into three main parts: when we're displaying
-	 * posts that are in the gallery category, when we're displaying
-	 * posts in the asides category, and finally all other posts.
-	 *
-	 * Additionally, we sometimes check for whether we are on an
-	 * archive page, a search page, etc., allowing for small differences
-	 * in the loop on each template without actually duplicating
-	 * the rest of the loop that is shared.
-	 *
-	 * Without further ado, the loop:
-	 */ ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
 		<div id="post-<?php the_ID(); ?>" <?php post_class('article-list-tease'); ?>>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 			<div class="entry-meta">
-				<?php //twentyten_posted_on(); ?>
-				Published <?php the_time('F j, Y \a\t g:i a') ?> by <?php the_author() ?> (updated <?php the_modified_time('F j, g:i a') ?>)
+				<span class="byline"><?php do_action( 'ode_author_posts_link' ); ?></span> - <span class="timestamp"><?php do_action( 'ode_timestamp' ); ?></span>
 			</div><!-- .entry-meta -->
 
 			<div class="entry-summary">
@@ -78,11 +41,7 @@
 			
 			<!-- .entry-summary -->
 
-			<?php //edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-
 		</div><!-- #post-## -->
-
-		<?php //comments_template( '', true ); ?>
 
 
 <?php endwhile; // End the loop. Whew. ?>
--- sidebar.php
+++ sidebar.php
@@ -1,82 +1,44 @@
-<?php
-/**
- * The Sidebar containing the primary and secondary widget areas.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
-?>
+<div id="primary" class="widget-area" role="complementary">
+	<ul class="xoxo">
 
-		<div id="primary" class="widget-area" role="complementary">
-			<ul class="xoxo">
+<?php if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
 
-<?php
-	/* When we call the dynamic_sidebar() function, it'll spit out
-	 * the widgets for that widget area. If it instead returns false,
-	 * then the sidebar simply doesn't exist, so we'll hard-code in
-	 * some default sidebar stuff just in case.
-	 */
-	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+		
+<?php endif; // end primary widget area ?>
 
-			<li id="search" class="widget-container widget_search">
-				<?php get_search_form(); ?>
-			</li>
-
-			<li id="archives" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
-
-		<?php endif; // end primary widget area ?>
-
-			<?php // GODUCKS VIDEO ADVERTISEMENT -- ONLY SHOW IF CATEGORY_NAME == SPORTS (ADDED BY IVAR, MAY 11, 2011)
-				  global $post;
-				  if (is_category('sports') || in_category('sports')) { // 9 is the ID for the Sports category... i think.
-			?>
-				<li id="goducks-sports-ad" class="widget-container">
-					<div style="width: 300px; height: 330px;">	
-					<div style="text-align: center; font-size: 10px; background: none repeat scroll 0% 0% rgb(255, 255, 255); padding: 5px 1px 1px;">ADVERTISEMENT</div>
-						<table width="300" cellspacing="0" cellpadding="0" border="0">
-						<tbody><tr><td>
-						<object width="300" height="318" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="iptvsyndicated">
-							<param value="http://www.goducks.com/mediaPortal/inline.swf" name="movie">
-							<param value="playerId=20507&amp;server=http://www.goducks.com/XML/titanv3/&amp;pageurl=http://www.goducks.com/mediaPortal/&amp;sitename=aff.oregon.dailyemerald&amp;locimage=http://image.cdnl3.xosnetwork.com/mediaPortal/&amp;jtv=500&amp;skin=500&amp;gaa=UA-8512810-1&amp;companion=true&amp;htmlid=iptvsyndicated&amp;brandTextColor=0xCCCCCC&amp;brandTextSelectedColor=0xFFFFFF&amp;autostart=true&amp;mute=true" name="flashVars">
-							<param value="high" name="quality">
-							<param value="true" name="allowFullScreen">
-							<param value="always" name="allowScriptAccess">
-							<embed width="300" height="318" flashvars="playerId=20507&amp;server=http://www.goducks.com/XML/titanv3/&amp;pageurl=http://www.goducks.com/mediaPortal/&amp;sitename=aff.oregon.dailyemerald&amp;locimage=http://image.cdnl3.xosnetwork.com/mediaPortal/&amp;jtv=500&amp;skin=500&amp;gaa=UA-8512810-1&amp;companion=true&amp;htmlid=iptvsyndicated&amp;brandTextColor=0xCCCCCC&amp;brandTextSelectedColor=0xFFFFFF&amp;autostart=true&amp;mute=true" allowscriptaccess="always" allowfullscreen="true" quality="high" type="application/x-shockwave-flash" src="http://www.goducks.com/mediaPortal/inline.swf" pluginspage="http://www.adobe.com/go/getflashplayer" name="iptvsyndicated">
-						</object>
-						</td></tr>
-						</tbody>
-						</table>
-					</div>
-				</li>
-			<?php } ?>
+	<?php // GODUCKS VIDEO ADVERTISEMENT -- ONLY SHOW IF CATEGORY_NAME == SPORTS (ADDED BY IVAR, MAY 11, 2011)
+		  if (is_category('sports') || in_category('sports')) { // 9 is the ID for the Sports category... i think.
+	?>
+		<li id="goducks-sports-ad" class="widget-container">
+			<div style="width: 300px; height: 330px;">	
+			<div style="text-align: center; font-size: 10px; background: none repeat scroll 0% 0% rgb(255, 255, 255); padding: 5px 1px 1px;">ADVERTISEMENT</div>
+				<table width="300" cellspacing="0" cellpadding="0" border="0">
+				<tbody><tr><td>
+				<object width="300" height="318" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="iptvsyndicated">
+					<param value="http://www.goducks.com/mediaPortal/inline.swf" name="movie">
+					<param value="playerId=20507&amp;server=http://www.goducks.com/XML/titanv3/&amp;pageurl=http://www.goducks.com/mediaPortal/&amp;sitename=aff.oregon.dailyemerald&amp;locimage=http://image.cdnl3.xosnetwork.com/mediaPortal/&amp;jtv=500&amp;skin=500&amp;gaa=UA-8512810-1&amp;companion=true&amp;htmlid=iptvsyndicated&amp;brandTextColor=0xCCCCCC&amp;brandTextSelectedColor=0xFFFFFF&amp;autostart=true&amp;mute=true" name="flashVars">
+					<param value="high" name="quality">
+					<param value="true" name="allowFullScreen">
+					<param value="always" name="allowScriptAccess">
+					<embed width="300" height="318" flashvars="playerId=20507&amp;server=http://www.goducks.com/XML/titanv3/&amp;pageurl=http://www.goducks.com/mediaPortal/&amp;sitename=aff.oregon.dailyemerald&amp;locimage=http://image.cdnl3.xosnetwork.com/mediaPortal/&amp;jtv=500&amp;skin=500&amp;gaa=UA-8512810-1&amp;companion=true&amp;htmlid=iptvsyndicated&amp;brandTextColor=0xCCCCCC&amp;brandTextSelectedColor=0xFFFFFF&amp;autostart=true&amp;mute=true" allowscriptaccess="always" allowfullscreen="true" quality="high" type="application/x-shockwave-flash" src="http://www.goducks.com/mediaPortal/inline.swf" pluginspage="http://www.adobe.com/go/getflashplayer" name="iptvsyndicated">
+				</object>
+				</td></tr>
+				</tbody>
+				</table>
+			</div>
+		</li>
+	<?php } ?>
 			
 			</ul>
 			
 		</div><!-- #primary .widget-area -->
 
-<?php
-	// A second sidebar for widgets, just because.
-	if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
+<?php if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
 
-		<div id="secondary" class="widget-area" role="complementary">
-			<ul class="xoxo">
-				<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-			</ul>
-		</div><!-- #secondary .widget-area -->
+	<div id="secondary" class="widget-area" role="complementary">
+		<ul class="xoxo">
+			<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
+		</ul>
+	</div><!-- #secondary .widget-area -->
 
 <?php endif; ?>
