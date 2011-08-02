debb81b0051a63b96fd5f767a64f2c9299dab94d
Daniel Bachhuber <d@danielbachhuber.com>
2011-08-02 15:34:29 +0000

    Remove Google Analytics gode

--- footer.php
+++ footer.php
@@ -51,19 +51,6 @@
 
 </div><!-- #wrapper -->
 
-<!-- ODE Google Analytics -->
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-22851831-1']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-   		var ga = document.createElement('script');     ga.type = 'text/javascript'; ga.async = true;
-    	ga.src = ('https:'   == document.location.protocol ? 'https://ssl'   : 'http://www') + '.google-analytics.com/ga.js';
-    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-</script>
-<!-- end ODE Google Analytics -->
-
 <!-- START CMN Google tag-->
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
@@ -90,6 +77,5 @@ pageTracker._trackPageview();
 
 	wp_footer();
 ?>
-<!--<?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?> seconds.-->
 </body>
 </html>
